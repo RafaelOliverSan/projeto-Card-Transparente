09e218681587ba55e5ed65effdc38b1fe56b90ca
RafaelOliverSan <153394151+RafaelOliverSan@users.noreply.github.com>
2024-04-19 08:02:39 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,19 @@
+window.revelar = ScrollReveal({reset:true})
+
+//criando efeito card1
+revelar.reveal('.card1',{
+  duration: 2000,
+  distance: '90px'
+})
+
+revelar.reveal('.card2',{
+  duration: 3000,
+  distance: '90px',
+  delay:1000
+})
+
+revelar.reveal('.card3',{
+  duration: 4000,
+  distance: '90px',
+  delay: 2000
+})
